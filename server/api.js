790d6a850d503ef44800ed168fe54901bc9abf4c
Graziele Almeida <graaziele.182@gmail.com>
2019-02-18 22:30:09 -0300

    Criando API

---
+++ server/api.js
@@ -0,0 +1,76 @@
+// Criando API
+// meteor create --full api
+
+if (Meteor.isServer) {
+
+    var Api = new Restivus({ // instalar meteor add nimble:restivus
+        useDefaultAuth: false,
+        prettyJson: true
+    });
+ 
+    // Get, Post, Put, Delete
+
+    Api.addRoute('users/:email', { authRequire: false }, {
+
+        delete: function () {
+
+            var user = Meteor.users.findOne({ //busca no bd da conta
+                'emails.address': this.urlParams.email //pegando o email do parametro
+            });
+
+            if (user) { //se encontrar o usuario
+                Meteor.users.remove(user._id); //remove o usuario
+
+                return {
+                    message: 'Usuario removido com sucesso!'
+                }
+            } else {
+                return {
+                    statusCode: 404,
+                    body: { message: 'Usuario não localizado!' }
+                }
+            }
+        }
+    })
+
+    Api.addRoute('users', { authRequire: false }, {
+
+        get: function () {
+            return {
+                statusCode: 200,
+                body: { message: 'API no ar :)' }
+            }
+        },
+
+        post: function () {
+
+            var Future = require('fibers/future');
+            var future = new Future();
+
+            var body = this.request.body; //pega o corpo da requisit
+            var response;
+  
+            var user = {
+                email: body.email,
+                password: body.password,
+                profile: { name: body.name }
+            }
+            
+            Meteor.call('saveAccount', user, function (err, res) {
+                if (err) {
+                    future.return({
+                        statusCode: 404,
+                        body: { message: err.reason }
+                    })
+                } else {
+                    console.log('tudo certo aqui.')
+                    future.return({
+                        statusCode: 200,
+                        body: { message: 'Usuario inserido com sucesso!' }
+                    });
+                }
+            })
+            return future.wait(response); //retorne quando o response tiver valor
+        }
+    })
+}
---
+++ server/main.js
@@ -0,0 +1,12 @@
+// Server entry point, imports all server code
+
+import '/imports/startup/server';
+import '/imports/startup/both';
+
+Meteor.methods({
+
+    'saveAccount'(user) {
+        return Accounts.createUser(user);
+    }
+
+});
